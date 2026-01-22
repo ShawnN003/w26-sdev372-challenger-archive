0b165f93e2b78aa0df6dd0f9072d3b3da0b93455
ShawnN003 <nguru.shawn@student.greenriver.edu>
2026-01-22 15:39:14 -0800

    installing Cors for the frontend and backend connection

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import sequelize from './db/db.js';
+import cors from 'cors'
+
 
 import matchesRouter from './routes/matchesRouter.js'
 import playerRouter from './routes/playerRouter.js'
@@ -7,6 +9,7 @@ import playerRouter from './routes/playerRouter.js'
 const app = express();
 const PORT = 3001;
 
+app.use(cors());
 app.use("/matches", matchesRouter());       
 app.use("/players", playerRouter());   
 
