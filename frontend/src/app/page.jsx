d42eef7ec5249ffd05dc7edd78b57a952472adc7
abdirashidexe <89213120+abdirashidexe@users.noreply.github.com>
2026-02-09 10:50:26 -0800

    api testing works

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -2,7 +2,10 @@ import Header from "./Components/Header";
 import Image from "next/image";
 import moneyGif from "./../../public/money.gif"
 
-export default function Home() {
+export default async function Home() {
+
+  const res = await fetch("http://localhost:3001/");
+  const data = await res.json();
   
   return (
     <>
@@ -12,6 +15,8 @@ export default function Home() {
         <p id="hero-desc">Where MMA & Pool Meet.</p>
         <div class="img-div">
           <Image id="moneyGif" src={moneyGif} alt="Money falling"></Image>
+          <h3><i>Testing if API works: {data.sport}</i></h3>
+          <img alt="thumbnail of sport" src={data.sportPic} width="250"></img>
         </div>
       </div>
     </>
