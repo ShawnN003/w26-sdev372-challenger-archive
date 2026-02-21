1d954b2855c7e105bfa39877d6ba804b5875be43
ShawnN003 <nguru.shawn@student.greenriver.edu>
2026-02-21 15:53:45 -0800

    Getting a percentage based calculation based on advantages and dissadvantages and outputting it in the frontend

--- backend/server/index.js
+++ backend/server/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-// import sequelize from './db/db.js';
 import cors from 'cors'
 
 
--- frontend/src/app/mmamatch/page.jsx
+++ frontend/src/app/mmamatch/page.jsx
@@ -6,23 +6,33 @@ import Header from "../Components/Header";
 export default function Home() {
   
   const [data, setData] = useState(null);
+  const [stats, setStats] = useState(null);
 
   useEffect(() => {
       fetch("http://localhost:3001/matches")
       .then(res => res.json())
       .then(data => setData(data))
       .catch(err => console.error(err));
+
+      fetch("http://localhost:3001/players/odds")
+      .then(res => res.text())
+      .then(data => setStats(data))
+      .catch(err => console.error(err));
   }, []);
 
-  if (!data) {
+  if (!data || !stats) {
     return <div>Loading...</div>;
   }
-  
+
   return (
     <>
       <Header />
-      <h1>MMAMatch</h1>
+      <h1>PoolMatch</h1>
       <span>{JSON.stringify(data)}</span>
+
+      <h2>Mock Stats</h2>
+      <span>{JSON.stringify(stats)}</span>
     </>
   );
+
 }
