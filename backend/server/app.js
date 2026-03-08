3611f42075d32a568b45566b0fe5be4d9949314b
DevRudy <55453823+SketchRudy@users.noreply.github.com>
2026-03-07 16:45:15 -0800

    Added integration and playwright testing

---
+++ backend/server/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mmaRouter from './routes/mmaRouter.js';
+import poolRouter from './routes/poolRouter.js';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/mma', mmaRouter);
+app.use('/pool', poolRouter);
+
+app.get('/', (req, res) => {
+  fetch('https://www.thesportsdb.com/api/v2/examples/all_sports.json', {
+    headers: { Accept: 'application/JSON' },
+  })
+    .then((r) => r.json())
+    .then((dataFromMyJSON) => {
+      console.log(dataFromMyJSON.all[2].strSport);
+      res.send({
+        sport: dataFromMyJSON.all[2].strSport,
+        sportPic: dataFromMyJSON.all[3].strSportThumb,
+      });
+    });
+});
+
+export default app;
--- backend/server/index.js
+++ backend/server/index.js
@@ -1,34 +1,6 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from "dotenv"
-dotenv.config()
+import app from './app.js';
 
-import mmaRouter from './routes/mmaRouter.js'
-import poolRouter from './routes/poolRouter.js'
-
-const app = express();
 const PORT = 3001;
-
-app.use(cors());
-app.use(express.json());
-app.use("/mma", mmaRouter);
-app.use("/pool", poolRouter);
-
-app.get("/", (req, res) => {
-    fetch("https://www.thesportsdb.com/api/v2/examples/all_sports.json", {
-        headers: {
-            Accept: "application/JSON"
-        }
-    })
-    .then(res => res.json())
-    .then(dataFromMyJSON => {
-        console.log(dataFromMyJSON.all[2].strSport),
-        res.send({
-            sport: dataFromMyJSON.all[2].strSport,
-            sportPic: dataFromMyJSON.all[3].strSportThumb
-        })
-    })
-});
 
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
